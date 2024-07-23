fdd4e2be727974566cedfdf5939fbc63ecef3a01
Yossi Bokor Bleile <yossi@yossi.eu>
2024-07-23 13:01:44 +0200

    Beta version of streamlit interface
    have written 'single_mode.py' and 'multi_mode.py' as streamlit interfaces, batch mode currenlty only available through the CLI.

---
+++ src/ToMA-gui.py
@@ -0,0 +1,39 @@
+#!python3
+##
+# @mainpage Topological Material Analysis (ToMA)
+# @authors Yossi Bokor Bleile
+# @version 0.4
+# @date February 2024
+# @copyright GPL
+# 
+# @section License
+# Topological Material Analysis (ToMA) is released under a GPL license. You should have received a [copy](LICENSE.md) of this when you downloaded this repository.
+# @section description_main Description
+# ToMA is dependent on Oineus and Diode to handle a lot of the "under the hood" functionalities.
+#
+# There are two ways you can use Amorphous Material Analysis:
+# -# Graphical User Interface (default)
+# -# Command Line Interface
+#    
+# both of which have several different modes:
+# -# single
+# -# multi
+# -# batch
+#
+# In the GUI, these are selected at startup, and for the CLI, the mode to be used is set in a settings file with an INI format. To use `ToMA` in the CLI mode, run 
+#`./ToMA.py -i c -s $SETTINGS FILE`.
+#
+# Information about the packages required and instructions for the settings files are in the [README](README.md).
+#
+# Copyright (c) 2023, 2024 Yossi Bokor Bleile.  All rights reserved.    
+
+
+import streamlit as st
+
+welcome_page = st.Page("welcome.py", title="ToMA")
+license_page = st.Page("license.py", title="License")
+single_mode = st.Page("single_mode.py", title="Single Mode")
+multi_mode = st.Page("multi_mode.py", title="Multi Mode")
+
+pg = st.navigation([welcome_page, single_mode, multi_mode, license_page])#[st.Page("welcome.py", title="ToMA"), st.Page("single_mode.py", title="Single Mode"), st.Page("license.py", title="License")])
+pg.run()
